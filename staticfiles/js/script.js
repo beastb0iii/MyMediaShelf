9f9eee4ffb1966bfce3727db57f2ea2d723dfefd
beastb0iii <reigenonizukacodes@gmail.com>
2025-05-27 22:14:21 +0800

    :camera_flash: UI Added: Modal

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,15 +1,38 @@
-// For Custom Select Arrow
-const select = document.getElementById("formDropdown");
+document.addEventListener("DOMContentLoaded", function () {
+	// For Custom Select Arrow
+	const select = document.getElementById("formDropdown");
 
-const closeArrow = () => {
-	setTimeout(() => {
-		select.classList.remove("open");
-	}, 100);
-};
+	if (select) {
+		const closeArrow = () => {
+			setTimeout(() => {
+				select.classList.remove("open");
+			}, 100);
+		};
 
-select.addEventListener("focus", () => {
-	select.classList.add("open");
+		select.addEventListener("focus", () => {
+			select.classList.add("open");
+		});
+
+		select.addEventListener("blur", closeArrow);
+		select.addEventListener("change", closeArrow);
+	}
+
+	// For modal
+
+	const addMediaItem = document.getElementById("addMediaItem");
+	const addModal = document.getElementById("addModal");
+	const closeModalBtn = document.getElementById("closeModalBtn");
+
+	addMediaItem.addEventListener("click", openAddModal);
+	closeModalBtn.addEventListener("click", closeAddModal);
+
+	function openAddModal() {
+		addModal.classList.add("active");
+		document.body.style.overflow = "hidden";
+	}
+
+	function closeAddModal() {
+		addModal.classList.remove("active");
+		document.body.style.overflow = "auto";
+	}
 });
-
-select.addEventListener("blur", closeArrow);
-select.addEventListener("change", closeArrow);
